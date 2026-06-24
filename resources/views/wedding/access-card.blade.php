0a3c8335ae6cbe519464aee0ba267b279f3c62b0
AnkiGilbertOkosso <ankigilbertokosso@gmail.com>
2026-06-24 09:30:12 +0100

    Comment out access card page div for layout adjustments in wedding view

--- resources/views/wedding/access-card.blade.php
+++ resources/views/wedding/access-card.blade.php
@@ -22,7 +22,7 @@
     @endphp
 
     {{-- items-start + modest top padding: less empty space above the card on tall screens --}}
-    <div class="access-card-page flex flex-col min-h-svh items-start justify-center px-3 pt-6 pb-12 sm:px-6 sm:pt-8 sm:pb-16">
+    {{-- <div class="access-card-page flex flex-col min-h-svh items-start justify-center px-3 pt-6 pb-12 sm:px-6 sm:pt-8 sm:pb-16">
         @include('wedding.partials.access-card-stage', ['guest' => $guest])
 
         @if ($guest->is_approved && $guest->qr_code)
@@ -37,5 +37,5 @@
                 <p class="text-xs text-wedding-muted" data-share-access-card-feedback aria-live="polite"></p>
             </div>
         @endif
-    </div>
+    </div> --}}
 @endsection
